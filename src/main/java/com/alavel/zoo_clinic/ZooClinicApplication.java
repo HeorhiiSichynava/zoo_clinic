749342f1407e130b3a521a72b4fc37727d78c367
HeorhiiSichynava <sichinava@gmail.com>
2021-08-17 11:03:42 +0300

    first commit. Add main packages to project. Add gitignore file

---
+++ src/main/java/com/alavel/zoo_clinic/ZooClinicApplication.java
@@ -0,0 +1,13 @@
+package com.alavel.zoo_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooClinicApplication.class, args);
+	}
+
+}
